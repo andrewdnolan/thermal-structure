506ed6d58167201a568a57aa08f89afdcce7cb56
andrewdnolan <andrew.d.nolan@maine.edu>
2021-09-28 20:44:46 -0700

    Fixed restart problem with enthalpy_timestep problem

--- study/enthalpy_timestep/enthalpy_timestep.sh
+++ study/enthalpy_timestep/enthalpy_timestep.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
-T=200  #[a]
+T=50  #[a]
 
-for dt_m in 1 2 6 12; do
+for dt_m in 1 2 6; do #12
   # Total number of timesteps
   NT=$(((T*12)/dt_m))
   dt_a=$(awk -v dt=$dt_m 'BEGIN {print dt/12.0}')
   NT_a=$((12/dt_m))
 
   # Place appropriate variables in
-  sed "s#<NT>#"$NT"#g;
+  sed "s#<T>#"${T}"#g;
+       s#<NT>#"$NT"#g;
        s#<dt_m>#"$dt_m"#g
        s#<H_dt>#"$dt_a"#g;
-       s#<NT_a>#"$NT_a"#g" "enthalpy_timestep.sif" > "20a_dt_${dt_m}m.sif"
+       s#<NT_a>#"$NT_a"#g" "enthalpy_timestep.sif" > "${T}a_dt_${dt_m}m.sif"
 
   # Run the model
-  ElmerSolver "20a_dt_${dt_m}m.sif"
+  ElmerSolver "${T}a_dt_${dt_m}m.sif"
 
   # Convert from .result to .nc
-  ../../src/elmer2nc/elmer2nc.sh  -r "./mesh/20a_dt_${dt_m}m.result" \
+  ../../src/elmer2nc/elmer2nc.sh  -r "./mesh/${T}a_dt_${dt_m}m.result" \
                                   -m "./mesh/" \
                                   -t "${NT}" \
                                   -o "./nc/"
 
-  rm "20a_dt_${dt_m}m.sif"
+  rm "${T}a_dt_${dt_m}m.sif"
 done
