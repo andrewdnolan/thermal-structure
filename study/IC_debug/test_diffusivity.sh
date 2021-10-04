4058c3609b645278307b73067a06adb24556729f
andrewdnolan <andrew.d.nolan@maine.edu>
2021-10-03 18:05:24 -0700

    with a constant temperate ice diffusion, the oscillations disapper. Unclear if cause of oscialltion is temperate ice diffusivity fortran code, or a numerical instability which is a function of diffusivity. more to look into

---
+++ study/IC_debug/test_diffusivity.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+NT=50
+
+
+for diff in "constant" "variable"; do
+
+  echo -e "------------------------------------------------------------------------\n" \
+          "Running test case for ${diff} temperate diffusivity\n"\
+          "------------------------------------------------------------------------\n" \
+          >> "test.out" 2>&1
+
+
+  if   [[ "$diff" == "constant" ]]; then
+
+    sed "s#<run>#"$diff"#g;
+         s#<diffusion>#real \#9.92e-4*spy#g" "diffusivity.sif" > "${diff}.sif"
+
+  elif [[ "$diff" == "variable" ]]; then
+    string="Variable Temperature \n \t\t\t Real procedure "../../bin/thermodynamics" "Diffusivity" "
+
+    sed "s#<run>#"$diff"#g;
+         s#<diffusion>#$string#g" "diffusivity.sif" > "${diff}.sif"
+  fi
+
+
+  #  Run the model
+  ElmerSolver "${diff}.sif" 2>>"test.out" | tee "logs/${diff}.log"
+
+
+  # Convert from .result to .nc
+  ../../src/elmer2nc/elmer2nc.sh  -r "./mesh/${diff}.result" \
+                                  -m "./mesh/" \
+                                  -t "${NT}" \
+                                  -o "./nc/" >> "test.out" 2>&1
+
+  rm "${diff}.sif"
+done
