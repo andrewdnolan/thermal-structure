3ed04a5927bb3b06a5f30fd622de368285351c0c
andrewdnolan <andrew.d.nolan@maine.edu>
2022-07-12 14:09:50 -0400

    rename virutalenv to proper name

--- config/build_py4elmer.sh
+++ config/build_py4elmer.sh
@@ -6,6 +6,11 @@
 module load python/3.8.10
 # Lazy scipy stack
 module load scipy-stack
+
+if [[ -d "$HOME/py4elmer_new" ]]; then
+  rm -r "$HOME/py4elmer_new"
+fi
+
 # create virtualenv
 virtualenv --no-download $HOME/py4elmer_new
 # activate env
