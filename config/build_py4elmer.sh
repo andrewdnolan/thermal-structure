01d8109839caee5f67aef9a98cb58e39547a92df
andrewdnolan <andrew.d.nolan@maine.edu>
2022-07-12 15:28:51 -0400

    whoops'

--- config/build_py4elmer.sh
+++ config/build_py4elmer.sh
@@ -7,17 +7,18 @@ module load python/3.8.10
 # Lazy scipy stack
 module load scipy-stack
 
-if [[ -d "$HOME/py4elmer_new" ]]; then
-  rm -r "$HOME/py4elmer_new"
+if [[ -d "$HOME/py4elmer" ]]; then
+  rm -r "$HOME/py4elmer"
 fi
 
 # create virtualenv
-virtualenv --no-download $HOME/py4elmer_new
+virtualenv --no-download $HOME/py4elmer
 # activate env
-source $HOME/py4elmer_new/bin/activate
+source $HOME/py4elmer/bin/activate
 # update pip
 pip install --no-index --upgrade pip
 # install the various packages needed ontop of scipy stack
-pip install xarray jupyterlab dask dask_jobqueue seaborn --no-index
+pip install --no-index pandas numpy scipy matplotlib ipython xarray jupyterlab \
+                       dask dask_jobqueue seaborn
 
 deactivate
