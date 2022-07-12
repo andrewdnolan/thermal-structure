eff45daccafb3519854f207fa2a340b78ec232d7
andrewdnolan <andrew.d.nolan@maine.edu>
2022-07-12 13:21:56 -0400

    build scirpt for python on cedar

---
+++ config/build_py4elmer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# script for building python environment on cedar
+
+# Load python 3
+module load python/3.8.10
+# Lazy scipy stack
+module load scipy-stack
+# create virtualenv
+virtualenv --no-download $HOME/py4elmer_new
+# activate env
+source $HOME/py4elmer_new/bin/activate
+# update pip
+pip install --no-index --upgrade pip
+# install the various packages needed ontop of scipy stack
+pip install xarray jupyterlab dask dask_jobqueue seaborn --no-index
+
+deactivate
