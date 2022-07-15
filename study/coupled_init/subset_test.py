106c4f75175fad5b0cbcd165ff098935e7dc7cea
andrewdnolan <andrew.d.nolan@maine.edu>
2022-07-15 14:15:40 -0400

    need first and last timesteps for volume calculations

--- study/coupled_init/subset_test.py
+++ study/coupled_init/subset_test.py
@@ -1,7 +1,7 @@
 import xarray as xr
 
 def preprocess(ds):
-    return ds.isel(t=-1)
+    return ds.isel(t=[0,-1])
 
 src_fp = 'glc1-a/nc/glc1-a_dx_50_NT_2000_dt_1.0_MB_*_OFF_Tma_*_prog_gridded.nc'
 gs_ds  = xr.open_mfdataset(src_fp, parallel=False, preprocess=preprocess)
