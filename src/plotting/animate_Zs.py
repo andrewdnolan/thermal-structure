4d5f13b9e3664c458df1ba91d90f45391a617725
andrewdnolan <andrew.d.nolan@maine.edu>
2022-07-27 11:05:42 -0400

    misc changes

--- src/plotting/animate_Zs.py
+++ src/plotting/animate_Zs.py
@@ -19,6 +19,124 @@ from matplotlib import animation, rc
 plt.rcParams['text.usetex']    = False
 plt.rcParams['animation.html'] = 'jshtml'
 
+def animate_2D_field(src,
+                     field,
+                     stride,
+                     interval=50,
+                     title='',
+                     cbar_label='',
+                     vmin=None,
+                     vmax=None,
+                     cmap=None,
+                     ):
+
+    if type(field) == tuple or type(field) == list:
+        n_fields = len(field)
+    else:
+        n_fields = 1
+        field    = [field]
+
+
+    fig, ax = plt.subplots(n_fields, 1,
+                           figsize=(9,3*n_fields),
+                           sharex=True,
+                           sharey=True,
+                           dpi=300,
+                           constrained_layout=True)
+
+    if n_fields == 1:
+        ax = [ax]
+
+    for j in range(n_fields):
+
+
+        if not vmin[j]:
+            vmin[j] = src[field[j]].min()
+        if not vmax[j]:
+            vmax[j] = src[field[j]].max()
+
+        kwargs = {#'levels' : np.linspace(vmin, vmax, 101),
+                        'vmin'   : vmin[j],
+                        'vmax'   : vmax[j],
+                        'cmap'   : cmap[j],
+                        'shading': 'gouraud'}
+
+        # Plot initial field
+        im = ax[j].pcolormesh(src.X[::-1]/1e3,
+                           src.Z.isel(t=0),
+                           src.isel(t=0)[field[j]],
+                           **kwargs)
+
+        # Set up the colorbar
+        cbar = fig.colorbar(im, ax=ax[j])
+        cbar.set_label(cbar_label[j], rotation=270, labelpad=20)
+
+        # Set axis limits
+        ax[j].set_ylim(src.Z.min(), src.Z.max())
+        ax[j].set_xlim(src.X.min()/1e3,
+                    src.X.max()/1e3)
+
+        ax[j].set_ylabel('Elevation (m a.s.l.)')
+
+        if j == n_fields-1:
+            ax[j].set_xlabel('Distance from divide (km)')
+        if j == 0:
+            ax[j].set_title(title)
+
+        # Plot the bed which is constant throughout animation
+        ax[j].plot(src.coord_1[::-1]/1e3,
+                src.isel(t=0,coord_2=-1).zbed,
+                color='k', label=r'$z_{\rm b}$')
+        # Fill between bed and bottom of plot
+        ax[j].fill_between(src.coord_1[::-1]/1e3,
+                        src.isel(t=0,coord_2=-1).zbed,
+                        y2=np.minimum(src.isel(t=0,coord_2=-1).zbed.min(), 0.0),
+                        color='gray', alpha=0.5)
+
+        if j == n_fields-1:
+            # Add time annotation
+            label = ax[j].text(0.9, 0.9, r"$t=0$",
+                            transform=ax[j].transAxes,
+                            ha='center', va='center')
+
+    def animate(i):
+
+        for j in range(n_fields):
+
+            kwargs = {#'levels' : np.linspace(vmin, vmax, 101),
+                            'vmin'   : vmin[j],
+                            'vmax'   : vmax[j],
+                            'cmap'   : cmap[j],
+                            'shading': 'gouraud'}
+            # Wipe the axis
+            ax[j].collections = []
+            # Plot the contour
+            ax[j].pcolormesh(src.X[::-1]/1e3,
+                          src.Z.isel(t=i),
+                          src.isel(t=i)[field[j]],
+                          **kwargs)
+
+            # Fill between bed and bottom of plot
+            ax[j].fill_between(src.coord_1[::-1]/1e3,
+                            src.isel(t=0,coord_2=-1).zbed,
+                            y2=np.minimum(src.isel(t=0,coord_2=-1).zbed.min(), 0.0),
+                            color='gray', alpha=0.5)
+
+            # Update time annotation
+            label.set_text("$t={{{:6.1f}}}$".format(src.t[i].values))
+
+
+    ax[j].plot(src.coord_1[::-1]/1e3,
+            src.isel(t=0, coord_2=-1).Z, 'k:', alpha=0.5, lw=1.0, label='$z_s(t=0.0)$')
+
+
+    ax[j].legend(loc='lower left')
+
+    anim = animation.FuncAnimation(
+        fig, animate, interval=interval, frames=np.arange(0,src.t.size, stride))
+
+    return fig, anim
+
 def animate_Zs(result, stride, title=''):
     fig, ax = plt.subplots(figsize=(9,3), constrained_layout=True)
 
--- study/coupled_init/cedar.sh
+++ study/coupled_init/cedar.sh
@@ -12,48 +12,7 @@
 # in the previous two lines %A" is replaced by jobID and "%a" with the array index
 
 #Load cedar module file
-# source ../../config/modulefile.cc.cedar
-
-parse_json()
-{
-  #-----------------------------------------------------------------------------
-  # Parse model parameters from input .json file needed for initialization
-  # perl code from: https://stackoverflow.com/a/27127626/10221482
-  #-----------------------------------------------------------------------------
-  # $1 (json_fp) --->   file path to json parameter file
-  #-----------------------------------------------------------------------------
-
-  # parse MB grid search start
-  MB_0=$( perl -MJSON -0lnE '
-    $params = decode_json $_;
-    say $params->{MB}->{range}->[0]
-    ' $1 )
-  # parse MB grid search stride
-  MB_s=$( perl -MJSON -0lnE '
-    $params = decode_json $_;
-    say $params->{MB}->{range}->[1]
-    ' $1 )
-  # parse MB grid search end
-  MB_f=$( perl -MJSON -0lnE '
-    $params = decode_json $_;
-    say $params->{MB}->{range}->[2]
-    ' $1 )
-  # parse horizontal gird spacing
-  dx=$(   perl -MJSON -0lnE '
-    $params = decode_json $_;
-    say $params->{dx}
-    ' $1 )
-  # parse MB curve fit type
-  FIT=$(   perl -MJSON -0lnE '
-    $params = decode_json $_;
-    say $params->{fit}
-    ' $1 )
-  # parse MB curve fit type
-  k=$(   perl -MJSON -0lnE '
-    $params = decode_json $_;
-    say $params->{k}
-    ' $1 )
-}
+source ../../config/modulefile.cc.cedar
 
 diagnostic_run()
 {
