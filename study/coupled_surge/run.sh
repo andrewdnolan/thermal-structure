58fe2cc0ee9fe08b846a5b9922007f7306f7a550
andrewdnolan <andrew.d.nolan@maine.edu>
2022-10-03 11:20:03 -0700

    try various betas and surge intervals

--- study/coupled_surge/run.sh
+++ study/coupled_surge/run.sh
@@ -29,47 +29,61 @@ pseudo_run()
 
 }
 
+
+
+full_pseudo()
+{
+  # Number of time interval based on dt
+  NT=$(awk -v dt=$dt -v t_f=$TT 'BEGIN {OFMT = "%.0f"; print (t_f/dt)}')
+
+  # prognostic run name
+  run_name="${KEY}_dx_${dx}_NT_${NT}_dt_${dt}_MB_${offset}_OFF_Tma_${T_ma}_B_${beta}_pseudo"
+
+  # run the transient model with diagnostic solution as restart fiedl
+  pseudo_run $dx $KEY $offset $run_name $SS_itters $restart $NT $dt
+
+  # grid the NetCDF file written by the NetcdfUGRIDOutputSolver
+  python3 ../../src/thermal/grid_data.py "result/${KEY}/nc/${run_name}.nc"      \
+                                 -out_fn "result/${KEY}/gridded/${run_name}.nc"
+
+  # dt=1.0
+  # NT=2000
+  # TT=2000
+  #
+  # # recovery run name
+  # RESTART="${run_name}.result"
+  # run_name="${KEY}_dx_${dx}_NT_${NT}_dt_${dt}_MB_${offset}_OFF_Tma_${T_ma}_B_${beta}_recovery"
+  #
+  # beta=1.0
+  # # run the transient model with diagnostic solution as restart fiedl
+  # pseudo_run $dx $KEY $offset $run_name $SS_itters $restart $NT $dt
+  #
+  # # grid the NetCDF file written by the NetcdfUGRIDOutputSolver
+  # python3 ../../src/thermal/grid_data.py "result/${KEY}/nc/${run_name}.nc"      \
+  #                               -out_fn "result/${KEY}/gridded/${run_name}.nc"
+
+}
+
+
 # parse the parameters from the json files
 parse_json "params/crmpt12.json"
 
 offset=-0.41
 T_ma=-8.5
 KEY='crmpt12'
-
-# limit to 10 S.S. itters for transient runs
-SS_itters=10
-
-dt=0.1
-NT=200
-TT=20
-beta=0.003
-
 # diagnostic run is now restart variable
 RESTART="crmpt12_dx_50_NT_2000_dt_1.0_MB_-0.41_OFF_Tma_-8.5_prog.result"
+# limit to 10 S.S. itters for transient runs
+SS_itters=10
+dt=0.05
 
-# # prognostic run name
-run_name="${KEY}_dx_${dx}_NT_${NT}_dt_${dt}_MB_${offset}_OFF_Tma_${T_ma}_B_${beta}_pseudo"
-# # run_name="test_noseasonality"
 
-# run the transient model with diagnostic solution as restart fiedl
-pseudo_run $dx $KEY $offset $run_name $SS_itters $restart $NT $dt
+# try various slip coefs
+for beta in 0.001 0.0005 0.0001; do
+  # try various surge intervals [yr]
+  for TT in 2 5 10; do
 
-# grid the NetCDF file written by the NetcdfUGRIDOutputSolver
-python3 ../../src/thermal/grid_data.py "result/${KEY}/nc/${run_name}.nc"      \
-                               -out_fn "result/${KEY}/gridded/${run_name}.nc"
-
-dt=1.0
-NT=100
-TT=100
-
-# recovery run name
-RESTART="${run_name}.result"
-run_name="${KEY}_dx_${dx}_NT_${NT}_dt_${dt}_MB_${offset}_OFF_Tma_${T_ma}_B_${beta}_recovery"
-
-beta=1.0
-# run the transient model with diagnostic solution as restart fiedl
-pseudo_run $dx $KEY $offset $run_name $SS_itters $restart $NT $dt
-
-# grid the NetCDF file written by the NetcdfUGRIDOutputSolver
-python3 ../../src/thermal/grid_data.py "result/${KEY}/nc/${run_name}.nc"      \
-                               -out_fn "result/${KEY}/gridded/${run_name}.nc"
+    # run the full pseduo surge simulation w/ current beta and surge length
+    full_pseudo
+  done
+done
