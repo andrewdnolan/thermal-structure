7eba17e6a4faef48c855868e8e41d21c9cf3442e
andrewdnolan <andrew.d.nolan@maine.edu>
2022-10-04 08:56:54 -0700

    fix restart loop problems

--- study/coupled_surge/run.sh
+++ study/coupled_surge/run.sh
@@ -56,13 +56,19 @@ full_pseudo()
   RESTART="${run_name}.result"
   run_name="${KEY}_dx_${dx}_NT_${NT}_dt_${dt}_MB_${offset}_OFF_Tma_${T_ma}_B_${beta}_recovery"
 
+  echo
+  echo $RESTART
+  echo
+  echo $run_name
+  echo
+
   beta=1.0
   # run the transient model with diagnostic solution as restart fiedl
-  pseudo_run $dx $KEY $offset $run_name $SS_itters $restart $NT $dt
-
-  # grid the NetCDF file written by the NetcdfUGRIDOutputSolver
-  python3 ../../src/thermal/grid_data.py "result/${KEY}/nc/${run_name}.nc"      \
-                                -out_fn "result/${KEY}/gridded/${run_name}.nc"
+  # pseudo_run $dx $KEY $offset $run_name $SS_itters $restart $NT $dt
+  #
+  # # grid the NetCDF file written by the NetcdfUGRIDOutputSolver
+  # python3 ../../src/thermal/grid_data.py "result/${KEY}/nc/${run_name}.nc"      \
+  #                               -out_fn "result/${KEY}/gridded/${run_name}.nc"
 
 }
 
@@ -79,10 +85,13 @@ RESTART="crmpt12_dx_50_NT_2000_dt_1.0_MB_-0.41_OFF_Tma_-8.5_prog.result"
 SS_itters=10
 dt=0.05
 
-# try various surge intervals [yr]
-for TT in 2 5 10; do
-  # try various slip coefs
-  for beta in 0.001 0.0005 0.0001; do
+
+# try various slip coefs
+for beta in 0.001 0.0005 0.0001; do
+  # try various surge intervals [yr]
+  for TT in 2 5 10; do
+    # dt needs to be defined in the loop
+    dt=0.05
 
     # run the full pseduo surge simulation w/ current beta and surge length
     full_pseudo
