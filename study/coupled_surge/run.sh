eb812ae617b5c9b89d57c81f3d8539dc714b1cdb
andrewdnolan <andrew.d.nolan@maine.edu>
2022-10-06 19:01:12 -0700

    beta value for midpoint in quadratic space

--- study/coupled_surge/run.sh
+++ study/coupled_surge/run.sh
@@ -41,12 +41,12 @@ full_pseudo()
   # prognostic run name
   run_name="${KEY}_dx_${dx}_NT_${NT}_dt_${dt}_MB_${offset}_OFF_Tma_${T_ma}_B_${beta}_pseudo"
 
-  # # run the transient model with diagnostic solution as restart fiedl
-  # pseudo_run $dx $KEY $offset $run_name $SS_itters $restart $NT $dt
-  #
-  # # grid the NetCDF file written by the NetcdfUGRIDOutputSolver
-  # python3 ../../src/thermal/grid_data.py "result/${KEY}/nc/${run_name}.nc"      \
-                                 # -out_fn "result/${KEY}/gridded/${run_name}.nc"
+  # run the transient model with diagnostic solution as restart fiedl
+  pseudo_run $dx $KEY $offset $run_name $SS_itters $restart $NT $dt
+
+  # grid the NetCDF file written by the NetcdfUGRIDOutputSolver
+  python3 ../../src/thermal/grid_data.py "result/${KEY}/nc/${run_name}.nc"      \
+                                 -out_fn "result/${KEY}/gridded/${run_name}.nc"
   beta_surge=$beta
   dt=1.0
   NT=2000
@@ -84,7 +84,8 @@ dt=0.05
 
 
 # try various slip coefs
-for beta in 0.001 0.0005 0.0001; do
+# for beta in 0.001 0.0005 0.0001; do
+for beta in 0.000707; do
   # try various surge intervals [yr]
   for TT in 2 5 10; do
     # dt needs to be defined in the loop
