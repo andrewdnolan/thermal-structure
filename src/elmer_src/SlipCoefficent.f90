5877932cd168c23be6aa1213165ad77dd704bd27
andrewdnolan <andrew.d.nolan@maine.edu>
2022-10-24 12:05:15 -0700

    pass time directly to function

--- src/elmer_src/SlipCoefficent.f90
+++ src/elmer_src/SlipCoefficent.f90
@@ -1,5 +1,5 @@
 !==============================================================================
-function periodic_surge(Model, Node, omega) result(beta)
+function periodic_surge(Model, Node, InputArray) result(beta)
 !==============================================================================
   ! This function returns the time dependent slip coefficent for periodic
   ! surge simulations.
@@ -17,7 +17,8 @@ function periodic_surge(Model, Node, omega) result(beta)
 
   type(Model_t) :: Model
   integer       :: Node            ! current node number
-  real(kind=dp) :: omega,        & ! water content fraction [--]
+  real(kind=dp) :: InputArray(2),& ! arguments passed to the function
+                   omega,        & ! water content fraction [--]
                    omega_thresh, & ! w.c. threshold to be considered temperate [--]
                    beta,         & ! slip coefficent [???]
                    S_period,     & ! duration of the active phase    [a]
@@ -31,6 +32,10 @@ function periodic_surge(Model, Node, omega) result(beta)
   ! TO DO: should read parameters values from the constants section.
   !@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+  ! copy input (should match the arguments!)
+  omega = InputArray(1)
+  time  = InputArray(2)
+
   ! 5 year surge, 45 year quiescesnce, for a 50 year surge period
   S_period = 5.0_dp
   Q_period = 45.0_dp
@@ -40,12 +45,12 @@ function periodic_surge(Model, Node, omega) result(beta)
   omega_thresh = 1.0e-3
 
   ! Get current timestep, which is really time at end of timestep
-  TimeVar  => VariableGet( Model % Mesh % Variables, "Time" )
+  ! TimeVar  => VariableGet( Model % Mesh % Variables, "Time" )
   ! Get current time [a]
-  Time    =   TimeVar % Values(1)
+  ! Time    =   TimeVar % Values(1)
 
   WRITE (Message, '(A,E10.2)')  "time=", Time
-  CALL INFO(Caller, Message, Level=0)
+  CALL INFO(Caller, Message, Level=1)
 
 
   ! Check if within surge period and that the bed is temperate
