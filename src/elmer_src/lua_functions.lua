5e82804f11f4d29bb7c4b1dcefd65715449c8e06
andrewdnolan <andrew.d.nolan@maine.edu>
2022-10-25 09:40:18 -0700

    switch to lua for periodic beta

--- src/elmer_src/lua_functions.lua
+++ src/elmer_src/lua_functions.lua
@@ -14,3 +14,25 @@ end
 function IfThenElse(condition,t,f)
   if condition then return t else return f end
 end
+
+
+function periodic_surge(omega, time)
+  -- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  -- 
+  -- periodic surge with hard coded periods and beta values
+  --
+  -- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+  S_p = 5          -- (s)urge      (p)eriod
+  Q_p = 45         -- (q)quiescent (p)eriod
+  C_p = S_p + Q_p  -- (c)ycle      (p)eriod
+
+  -- check if within surge period and that the bed is temperate
+  if (time % C_p < S_p) and (omega >= 0.001) then
+    beta = 0.001
+  else
+    beta = 1.000
+  end
+
+  return beta
+end
