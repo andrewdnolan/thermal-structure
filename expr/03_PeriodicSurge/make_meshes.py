934f2b4463137a3aa38e67845017749e0c205ac4
andrewdnolan <andrew.d.nolan@maine.edu>
2022-11-03 22:00:45 -0700

    build out periodic surge repo

---
+++ expr/03_PeriodicSurge/make_meshes.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+sys.path.append('../../src')
+import thermal.mesh as meshing
+
+# input glaciers and desired mesh resolutions for coarse grid searches
+glaciers = { # small
+             'crmpt12'   : 50,  # Δx [m]
+             # # medium
+             # 'klun-a'    : 100, # Δx [m]
+            }
+
+
+# filepath relative to top of git repo
+rel_fp = "./expr/00_CoupledInit/result"
+
+for key in glaciers.keys():
+    Δx     = glaciers[key]
+    out_fp = os.path.join(rel_fp, key, "mesh_dx{}".format(Δx))
+    nc_fp  = f'result/{key}/nc/'
+    gridded_fp = f'result/{key}/gridded/'
+
+    #-----
+    # Mesh
+    #-----
+    # Make the mesh
+    stat = meshing.make_mesh(key , Δx, out_fp)
+
+    #-------
+    # NetCDF
+    #-------
+    for nc_folder in [nc_fp, gridded_fp]:
+        # Make the NetCDF folder
+        if not os.path.exists(nc_folder):
+            os.mkdir(nc_folder)
+
+        # Add .gitkeep file to NetCDF folder so dir struc is preserved on GitHub
+        if not os.path.exists(os.path.join(nc_folder, '.gitkeep')):
+            with open(os.path.join(nc_folder, '.gitkeep'), 'w') as f:
+                pass
+
+    #--------
+    # figures
+    #--------
+    # Make figures dir
+    if not os.path.exists(f'figs/'):
+        os.mkdir('figs/')
+    if not os.path.exists(f'figs/{key}/'):
+        os.mkdir(f'figs/{key}/')
+
+
+    # Add .gitkeep file to figures folder so dir struc is preserved on GitHub
+    if not os.path.exists(os.path.join(f'figs/{key}/', '.gitkeep')):
+        with open(os.path.join(f'figs/{key}/', '.gitkeep'), 'w') as f:
+            pass
+
+
+    #-----
+    # logs
+    #-----
+    # Make logs dir
+    if not os.path.exists(f'logs/'):
+        os.mkdir('logs/')
+    if not os.path.exists(f'logs/{key}/'):
+        os.mkdir(f'logs/{key}/')
+
+
+    # Add .gitkeep file to logs folder so dir struc is preserved on GitHub
+    if not os.path.exists(os.path.join(f'logs/{key}/', '.gitkeep')):
+        with open(os.path.join(f'logs/{key}/', '.gitkeep'), 'w') as f:
+            pass
+
+    #-----
+    # run
+    #-----
+    # Make run dir
+    if not os.path.exists(f'run/'):
+        os.mkdir('run/')
+
+    # Add .gitkeep file to run folder so dir struc is preserved on GitHub
+    if not os.path.exists(os.path.join(f'run/', '.gitkeep')):
+        with open(os.path.join(f'run/', '.gitkeep'), 'w') as f:
+            pass
---
+++ expr/03_PeriodicSurge/periodic_surging.sh
@@ -0,0 +1,274 @@
+#!/usr/bin/env bash
+
+evenly_divisible()
+{ # helper function to determine if two numbers are evenly divisible
+  #               1 : true  | 0 : false
+  echo $(awk -v a=$1 -v b=$2 'BEGIN{print((a / b % 1) == 0)}')
+}
+
+cycle_period()
+{ # perform float addition
+  echo $(awk -v a=$1 -v b=$2 'BEGIN{printf("%.3f", a + b)}')
+}
+
+number_of_cycles()
+{ # Calculate the number of surge cycles for input params:
+  ##############################################################################
+  # $1 --> simulation length  [a]
+  # $2 --> surge cycle period [a]
+  ##############################################################################
+
+  # test if simulation length is evenly divisible by the surge cycle period
+  local remain=$(evenly_divisible $1 $2)
+
+  # if not evenly divisible throw error
+  if [ $remain -ne 1 ]; then
+    echo
+    echo "ERROR simulation length not evenly divisible by surge cycle"
+    echo
+    exit 1
+  fi
+
+  # divide the simulation length by the surge cycle period
+  echo $(awk -v a=$1 -v b=$2 'BEGIN{printf("%.0f", a / b)}')
+}
+
+fill_timestep_sizes()
+{ # fill the timestep_size array
+  ##############################################################################
+  # $1 --> number of surge cycles
+  # $2 --> surging   timestep [a]
+  # $3 --> quiescent timestep [a]
+  ##############################################################################
+
+  # timestep array lenghts
+  local M=$((2*$1))
+
+  # initialize the empty array
+  local timestep_sizes=()
+
+  for ((i = 1 ; i <= M ; i+=2)); do
+    timestep_sizes+=($2)
+    timestep_sizes+=($3)
+  done
+
+  # return the timestep sizes array
+  echo "${timestep_sizes[*]}"
+}
+
+fill_timestep_intervals()
+{ # fill the timestep_intervals array
+  ##############################################################################
+  # $1 --> number of surge cycles
+  # $2 --> surging   timestep [a]
+  # $3 --> quiescent timestep [a]
+  # $4 --> surging   period   [a]
+  # $5 --> quiescent period   [a]
+  ##############################################################################
+
+  # test if simulation length is evenly divisible by the surge cycle period
+  local surge_remain=$(evenly_divisible $4 $2)
+  local quisc_remain=$(evenly_divisible $5 $3)
+
+  # if not evenly divisible throw error
+  if [ $surge_remain -ne 1 ]; then
+    echo
+    echo "ERROR: surge period not evenly divisible by surging timestep"
+    echo
+    exit 1
+  elif [ $quisc_remain -ne 1 ]; then
+    echo
+    echo "ERROR: quiescent period not evenly divisible by quiescent timestep"
+    echo
+    exit 1
+  fi
+
+  # timestep array lenghts
+  local M=$((2*$1))
+
+  # initialize the empty array
+  local timestep_intervals=()
+
+  for ((i = 1 ; i <= M ; i+=2)); do
+    timestep_intervals+=($(awk -v a=$4 -v b=$2 'BEGIN{printf("%.0f", a / b)}'))
+    timestep_intervals+=($(awk -v a=$5 -v b=$3 'BEGIN{printf("%.0f", a / b)}'))
+  done
+
+  # return the timestep sizes array
+  echo "${timestep_intervals[*]}"
+}
+
+fill_dynamicexec_intervals()
+{ # fill the timestep_intervals array
+  ##############################################################################
+  # $1 --> number of surge cycles
+  # $2 --> dynamic surging   timestep [a]
+  # $3 --> dynamic quiescent timestep [a]
+  # $4 --> thermal surging   timestep [a]
+  # $5 --> thermal quiescent timestep [a]
+  ##############################################################################
+
+  # test if simulation length is evenly divisible by the surge cycle period
+  local surge_remain=$(evenly_divisible $2 $4)
+  local quisc_remain=$(evenly_divisible $3 $5)
+
+  # if not evenly divisible throw error
+  if [ $surge_remain -ne 1 ]; then
+    echo
+    echo "ERROR (surging)  : dynamic dt not evenly divisible by quiescent dt"
+    echo
+    exit 1
+  elif [ $quisc_remain -ne 1 ]; then
+    echo
+    echo "ERROR (quiescent): dynamic dt not evenly divisible by quiescent dt"
+    echo
+    exit 1
+  fi
+
+  # timestep array lenghts
+  local M=$((2*$1))
+
+  # initialize the empty array
+  local dynamicexec_intervals=()
+
+  for ((i = 1 ; i <= M ; i+=2)); do
+    dynamicexec_intervals+=($(awk -v a=$2 -v b=$4 'BEGIN{printf("%.0f", a / b)}'))
+    dynamicexec_intervals+=($(awk -v a=$3 -v b=$5 'BEGIN{printf("%.0f", a / b)}'))
+  done
+
+  # return the timestep sizes array
+  echo "${dynamicexec_intervals[*]}"
+}
+
+calc_number_of_timesteps()
+{ # sum the timestep array to calculate the number of timesteps
+  sum=0
+  for i in ${1[@]}; do
+    let sum+=$i
+  done
+
+  echo $sum
+}
+parse_json()
+{ # Parse model parameters from .json config file
+
+  ##############################################################################
+  # $1 (json_fp) --->   file path to json parameter file
+  ##############################################################################
+
+  # parse reference MB value
+  MB=$( perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{MB}
+    ' $1 )
+  # parse reference T_ma value
+  T_ma=$( perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{T_ma}
+    ' $1 )
+  # surging dynamic timestep
+  SD_dt=$( perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{SD_dt}
+    ' $1 )
+  # quiescent dynamic timestep
+  QD_dt=$( perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{QD_dt}
+    ' $1 )
+  # surging thermal timestep
+  ST_dt=$( perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{ST_dt}
+    ' $1 )
+  # quiescent thermal timestep
+  QT_dt=$( perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{QT_dt}
+    ' $1 )
+  # parse horizontal gird spacing
+  dx=$(   perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{dx}
+    ' $1 )
+  # parse timestep size
+  dt=$(   perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{dt}
+    ' $1 )
+  # number of time integration steps
+  TT=$(   perl -MJSON -0lnE '
+    $params = decode_json $_;
+    say $params->{TT}
+    ' $1 )
+}
+
+periodic_run()
+{
+  # Update the .SIF FILE with the model run specifc params
+  sed "s#<M>#"$M"#g;
+       s#<DX>#"$dx"#g;
+       s#<KEY>#"$KEY"#g;
+       s#<beta>#"$beta"#g;
+       s#<T_mean>#"$T_ma"#g;
+       s#<offset>#"$offset"#g;
+       s#<dt_arr>#""${dt_arr[*]}""#g;
+       s#<NT_arr>#""${NT_arr[*]}""#g;
+       s#<RESTART>#"$RESTART"#g
+       s#<run_name>#"$run_name"#g;
+       s#<SS_itters>#"$SS_itters"#g;
+       s#<dyn_exec_int>#""${dyn_exec_arr[*]}""#g;" "./sifs/periodic_surge.sif" > "./sifs/${run_name}.sif"
+
+  # filepath to log file
+  log_file="${KEY}/logs/${run_name}.log"
+
+  # Run the model
+  # docker exec elmerenv /bin/sh -c "cd shared_directory/Thesis/thermal-structure/study/coupled_surge;
+  #                                  ElmerSolver ./sifs/${run_name}.sif "
+
+  ElmerSolver ./sifs/${run_name}.sif
+
+  # Remove the sif file
+  rm "./sifs/${run_name}.sif"
+
+}
+
+# parse the parameters from the json files
+parse_json "params/crmpt12.json"
+
+offset=-0.41
+T_ma=-8.5
+KEY='crmpt12'
+# diagnostic run is now restart variable
+RESTART="crmpt12_dx_50_NT_100_dt_0.05_MB_-0.41_OFF_Tma_-8.5_B_0.001_pseudo_dt_1.0_NT_2000_recovery.result"
+# limit to 10 S.S. itters for transient runs
+SS_itters=10
+
+beta=0.001
+
+# run the model for 2000 years
+TT=2100
+
+
+# Surge intervals
+S_P=5.00                       # (s)urge  (p)eriod
+Q_P=25.0                       # (q)uies. (p)eriod
+# calculate surge cycle period; from the set surge/quiescent interval
+C_P=$(cycle_period $S_P $Q_P)  # (c)ycle  (p)eriod
+# number of surge cycles
+NC=$(number_of_cycles $TT $C_P)
+# timestep array lenghts
+M=$((2*NC))
+
+# get the timestep sizes array
+dt_arr=($(fill_timestep_sizes $NC $ST_dt $QT_dt))
+# get the timestep intervals array
+NT_arr=($(fill_timestep_intervals $NC $ST_dt $QT_dt $S_P $Q_P))
+# get the array of intervals to execute the dynamic solver at
+dyn_exec_arr=($(fill_dynamicexec_intervals $NC $SD_dt $QD_dt $ST_dt $QT_dt))
+
+# prognostic run name
+run_name="${KEY}_dx_${dx}_TT_${TT}_MB_${offset}_OFF_Tma_${T_ma}_B_${beta}_SP_${S_P}_QP_${Q_P}"
+
+# create file form template
+periodic_run
