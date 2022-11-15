b4cd3f15a0c26623f16a6a7c8eba826c95e2c689
andrewdnolan <andrew.d.nolan@maine.edu>
2022-11-15 15:52:39 -0800

    prep for longer runs

--- expr/03_PeriodicSurge/run/crmpt12/crmpt12_submit.sh
+++ expr/03_PeriodicSurge/run/crmpt12/crmpt12_submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --array=1-6%6
-#SBATCH --job-name=crmpt12_%A                      # base job name for the array
-#SBATCH --mem-per-cpu=4G                        # maximum 200M per job
-#SBATCH --time=75:00:00                                # maximum walltime per job
+#SBATCH --array=1-3%3
+#SBATCH --job-name=crmpt12_periodic                # base job name for the array
+#SBATCH --mem-per-cpu=4G                           # maximum 200M per job
+#SBATCH --time=232:00:00                           # maximum walltime per job
 #SBATCH --nodes=1                                  # Only one node is needed
 #SBATCH --ntasks=1                                 # These are serial jobs
 #SBATCH --mail-type=ALL                            # send all mail (way to much)
