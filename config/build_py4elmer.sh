6096de0e2ea0ebd46b1594b881c38ec0a9b271f9
andrewdnolan <andrew.d.nolan@maine.edu>
2023-01-24 16:12:43 -0500

    update cedar python env for dask gridding and zarr writing

--- config/build_py4elmer.sh
+++ config/build_py4elmer.sh
@@ -3,22 +3,22 @@
 # script for building python environment on cedar
 
 # Load python 3
-module load python/3.8.10
+module load python/3.10.2
 # Lazy scipy stack
 module load scipy-stack
 
-if [[ -d "$HOME/py4elmer" ]]; then
-  rm -r "$HOME/py4elmer"
+if [[ -d "$HOME/python_envs/py4elmer" ]]; then
+  rm -r "$HOME/python_envs/py4elmer"
 fi
 
 # create virtualenv
-virtualenv --no-download $HOME/py4elmer
+virtualenv --no-download $HOME/python_envs/py4elmer
 # activate env
-source $HOME/py4elmer/bin/activate
+source $HOME/python_envs/py4elmer/bin/activate
 # update pip
 pip install --no-index --upgrade pip
 # install the various packages needed ontop of scipy stack
-pip install --no-index xarray jupyterlab dask dask_jobqueue bokeh seaborn
+pip install --no-index xarray jupyterlab dask dask_jobqueue bokeh seaborn zarr distributed==2022.9.2
 
 # Steps three and four form https://docs.alliancecan.ca/wiki/Advanced_Jupyter_configuration
 echo -e '#!/bin/bash\nunset XDG_RUNTIME_DIR\njupyter lab --ip $(hostname -f) --no-browser' \
