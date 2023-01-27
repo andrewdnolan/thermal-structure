70e6f7b922d2b75bc26f6ca17e10f97a9422ca43
andrewdnolan (westgrid) <andrew.d.nolan@maine.edu>
2023-01-27 06:59:56 -0800

    remove pinned dask distrubted version

--- config/build_py4elmer.sh
+++ config/build_py4elmer.sh
@@ -18,7 +18,7 @@ source $HOME/python_envs/py4elmer/bin/activate
 # update pip
 pip install --no-index --upgrade pip
 # install the various packages needed ontop of scipy stack
-pip install --no-index xarray jupyterlab dask==2022.9.2 dask_jobqueue bokeh seaborn zarr distributed==2022.9.2
+pip install --no-index xarray jupyterlab dask_jobqueue bokeh seaborn zarr distributed
 
 # Steps three and four form https://docs.alliancecan.ca/wiki/Advanced_Jupyter_configuration
 echo -e '#!/bin/bash\nunset XDG_RUNTIME_DIR\njupyter lab --ip $(hostname -f) --no-browser' \
