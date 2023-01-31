2e60b77d10eec6d3f42698217f45431acc191aa7
andrewdnolan <andrew.d.nolan@maine.edu>
2023-01-31 17:37:17 -0500

    fix end of line typos

--- expr/00_CoupledInit/run/crmpt12/patch_postproc.sh
+++ expr/00_CoupledInit/run/crmpt12/patch_postproc.sh
@@ -6,7 +6,7 @@ parse_params()
     <<< $(sed -n "${1}p" ./run/crmpt12/gridsearch.commands | cut -d " " -f 13,15)
 }
 
-mkdir -p "sub_from_0.5_to_3000_by_10_YEARS/nc/"
+mkdir -p "result/crmpt12/sub_from_0.5_to_3000_by_10_YEARS/nc/"
 mkdir -p "result/crmpt12/sub_from_0.5_to_3000_by_10_YEARS/zarr/"
 
 parse_params 10
@@ -21,7 +21,7 @@ run_name="crmpt12_dx_50_NT_30000_dt_0.1_MB_${offset}_OFF_Tma_${T_ma}_prog"
 
 # run the subsampling script
 downsample -i "./result/crmpt12/nc/${run_name}.nc" \
-           -o "result/crmpt12/sub_from_0.5_to_3000_by_10_YEARS/nc/${run_name}.nc" \ 
+           -o "./result/crmpt12/sub_from_0.5_to_3000_by_10_YEARS/nc/${run_name}.nc" \
            --start 4 --stop -1 --stride 100
 
 # grid the NetCDF file written by the NetcdfUGRIDOutputSolver
