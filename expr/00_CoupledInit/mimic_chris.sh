50b41bcf1b2682c632926f7c9657d13c95dd6327
andrewdnolan <andrew.d.nolan@maine.edu>
2023-02-02 12:43:04 -0500

    try gridding in exact replica of chris"s demo which now works"

--- expr/00_CoupledInit/mimic_chris.sh
+++ expr/00_CoupledInit/mimic_chris.sh
@@ -3,9 +3,9 @@
 #SBATCH --time=00:15:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=2000M
-#SBATCH --output=copy_chris_%A.out   # standard output
-#SBATCH --error=copy_chris_%A.err    # standard error
+#SBATCH --mem-per-cpu=4000M
+#SBATCH --output=mimic_chris_%A.out   # standard output
+#SBATCH --error=mimic_chris_%A.err    # standard error
 
 
 module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3 python/3.8.10 scipy-stack
@@ -28,4 +28,5 @@ dask-worker --scheduler-file $SCHEDULER_FILE --no-dashboard --no-nanny --nworker
 touch in
 touch out
 
-PYTHONPATH=$PYTHONPATH:../../src/thermal python3 ../../src/thermal/grid_data.py in -out_fn out
+PYTHONPATH=$PYTHONPATH:../../src/thermal python3 "../../src/thermal/grid_data.py "./result/crmpt12/nc/crmpt12_dx_50_NT_30000_dt_0.1_MB_-0.50_OFF_Tma_-8.5_prog.nc" \
+                                    -out_fn "./result/crmpt12/gridded/crmpt12_dx_50_NT_30000_dt_0.1_MB_-0.50_OFF_Tma_-8.5_prog.zarr"
