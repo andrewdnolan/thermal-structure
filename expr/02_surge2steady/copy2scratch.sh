c2f798c1bc25d46e2059c60f244df494b25b67ae
andrewdnolan <andrew.d.nolan@maine.edu>
2023-03-31 16:32:18 -0400

    prep repo for cedar production runs

---
+++ expr/02_surge2steady/copy2scratch.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# directory on the scratch file system where jobs will be run
+dest="/home/anolan/scratch/thermal-structure/expr/02_surge2steady/"
+
+# copy the source files
+rsync -upE surge2steady.* "${dest}/"
+
+# Whole directories to copy to scratch
+for dir in "run/" "sifs/" "params/"; do
+	rsync -rupE --delete $dir $dest/$dir
+done
+
+# folder structures to copy to scratch
+for dir in "logs/" "result/"; do
+	rsync -ar --filter="-! */" $dir $dest/$dir
+done
+
+# copy the mesh files
+rsync -upaR result/*/*/mesh.* $dest
+# copy whatever restrat files are included in the git repo
+rsync -upaR result/*/*/*.result $dest
+
+
+# make symbolic links to the time profile files
+for f in $(find ./ -name "*.surge2steady.time_profile"); do
+	# remove local path (./) from filename
+	file=${f/.\//}
+
+	# check if the symbolic link has already been created
+	if [ ! -L "${dest}/${file}" ]; then
+		# if not, create the symlink
+		ln -s "${PWD}/${file}" "${dest}/${file}"
+	fi
+
+done
