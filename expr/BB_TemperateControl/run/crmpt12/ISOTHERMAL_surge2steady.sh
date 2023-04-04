33dbd5e8cae858baf11037248b7cce8953d5793d
andrewdnolan <andrew.d.nolan@maine.edu>
2023-04-04 17:59:44 -0400

    fix bugs and make executable

--- expr/BB_TemperateControl/run/crmpt12/ISOTHERMAL_surge2steady.sh
+++ expr/BB_TemperateControl/run/crmpt12/ISOTHERMAL_surge2steady.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+dx=50         # horizontal gridcell spacing [m]
 SP=2          # surge period
 QP=2000       # recovery (i.e. quiescent) period
 S_dt=0.05     # surging timestep  [a]
@@ -9,7 +10,7 @@ T_ma=0.0      # Dummy value for the air temperature
 
 
 ./surge2steady.py -k $key -dx $dx -SP $SP -QP 100 -beta 1e-4 -SD_dt $S_dt -ST_dt $S_dt \
-                    -QT_dt $Q_dt -QD_dt $Q_dt -z_lim 2400 -offset -0.444 -T_ma $T_ma
+                    -QT_dt $Q_dt -QD_dt $Q_dt -z_lim 2400 -offset -0.444 -T_ma $T_ma \
                     -RESTART "crmpt12_dx_50_NT_3000_dt_1.0_MB_-0.444_OFF_ISOTHERM_prog.result"
 
 # #corresponds to np.logspace(-3,-4, 9)
