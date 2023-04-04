65ce9acbe121d798cc04dad19b78f7e949b07b51
andrewdnolan <andrew.d.nolan@maine.edu>
2023-04-04 18:03:23 -0400

    fix bugs and make executable

--- expr/BB_TemperateControl/run/crmpt12/ISOTHERMAL_surge2steady.sh
+++ expr/BB_TemperateControl/run/crmpt12/ISOTHERMAL_surge2steady.sh
@@ -7,10 +7,10 @@ S_dt=0.05     # surging timestep  [a]
 Q_dt=1.0      # recovery timestep [a]
 key="crmpt12" # glacier identifier
 T_ma=0.0      # Dummy value for the air temperature
-
+offset=-0.444    # mass balance anomoly [m a-1]
 
 ./surge2steady.py -k $key -dx $dx -SP $SP -QP 100 -beta 1e-4 -SD_dt $S_dt -ST_dt $S_dt \
-                    -QT_dt $Q_dt -QD_dt $Q_dt -z_lim 2400 -offset -0.444 -T_ma $T_ma \
+                    -QT_dt $Q_dt -QD_dt $Q_dt -z_lim 2400 -off $offet -T_ma $T_ma \
                     -RESTART "crmpt12_dx_50_NT_3000_dt_1.0_MB_-0.444_OFF_ISOTHERM_prog.result"
 
 # #corresponds to np.logspace(-3,-4, 9)
