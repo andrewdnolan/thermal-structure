df3335cfeddde9009f13f3acc974bf50bd72f795
andrewdnolan <andrew.d.nolan@maine.edu>
2023-04-05 14:51:32 -0400

    update production runs to be done on cedar

--- expr/03_PeriodicSurge/make_experiment.py
+++ expr/03_PeriodicSurge/make_experiment.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
+import numpy as np
 from itertools import product
 
 # glacier identifer
 key  = 'crmpt12'
 # simulation length [yr]
 TT   = 3000
-# slip coeffiecent  [???]
-beta = 0.001
-# walltime in hh:mm:ss
-WT   = '75:00:00'
+# walltime in dd:hh:mm:ss
+WT   = '10:00:00:00'
 # memory allocation
 MEM  = '4G'
+# fixed 2-year surge period [a]
+SP = 2
+# slip coefficients to test (3 order of magnitude)
+betas  = np.logspace(-6, -8, 9)**0.5
+# length of the surge cycle [a]
+cycles = [15, 30, 60]
+
 
 # base command used to execute a single simulation
-cmd = "./periodic_surge.py -k \"{KEY}\" -SP {SP} -QP {QP} -beta {beta} -TT {TT}"
+cmd = "./periodic_surge.py -k \"{KEY}\" -SP {SP} -QP {QP} -beta {beta:1.3e} -TT {TT}"
 
 # open the commands file used by the job array
 with open(f'./run/{key}/{key}.commands', 'w') as f:
 
-    # itterate over active phase and surge-cycle periods [yr]
-    for i, (SP, CP) in enumerate(product([2,5], [15, 30, 60])):
+    # itterate over betas and surge-cycle periods [yr]
+    for i, (beta, CP) in enumerate(product(betas, cycles)):
 
         # calculate quiescent period [yr]
         QP = CP-SP
