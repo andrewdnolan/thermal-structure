b8b28ee96ae5dd2ace584122175dbea2ae17eadc
andrewdnolan <andrew.d.nolan@maine.edu>
2023-05-26 17:25:22 -0400

    run longer for recovery

--- expr/BB_TemperateControl/run/crmpt12/ISOTHERMAL_surge2steady.sh
+++ expr/BB_TemperateControl/run/crmpt12/ISOTHERMAL_surge2steady.sh
@@ -2,7 +2,7 @@
 
 dx=50         # horizontal gridcell spacing [m]
 SP=2          # surge period
-QP=2000       # recovery (i.e. quiescent) period
+QP=4000       # recovery (i.e. quiescent) period
 S_dt=0.05     # surging timestep  [a]
 Q_dt=1.0      # recovery timestep [a]
 key="crmpt12" # glacier identifier
@@ -13,8 +13,17 @@ offset=-0.444    # mass balance anomoly [m a-1]
 betas=(1.000e-03 7.499e-04 5.623e-04 4.217e-04 3.162e-04 
        2.371e-04 1.778e-04 1.334e-04 1.000e-04)
 
+# first surge cycle
 for beta in ${betas[@]}; do
     ./surge2steady.py -k $key -dx $dx -SP $SP -QP $QP -beta $beta -SD_dt $S_dt -ST_dt $S_dt \
                         -QT_dt $Q_dt -QD_dt $Q_dt -z_lim 2400 -off $offset -T_ma $T_ma \
                         -RESTART "crmpt12_dx_50_NT_3000_dt_1.0_MB_-0.444_OFF_Tma_0.0_prog.result"
 done
+
+# # second surge cycle
+# for beta in ${betas[@]}; do
+#     ./surge2steady.py -k $key -dx $dx -SP $SP -QP $QP -beta $beta -SD_dt $S_dt -ST_dt $S_dt \
+#                         -QT_dt $Q_dt -QD_dt $Q_dt -z_lim 2400 -off $offset -T_ma $T_ma \
+#                         -RESTART "crmpt12_dx_50_NT_3000_dt_1.0_MB_-0.444_OFF_Tma_0.0_prog.result"
+#                         -cycle2 -RESTART "crmpt12_dx_50_NT_40_dt_0.05_MB_-0.37_OFF_Tma_${T_ma}_B_${beta}_pseudo_NT_2000_recovery.result"
+# done
