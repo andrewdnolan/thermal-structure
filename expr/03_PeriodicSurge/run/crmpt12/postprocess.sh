fbcaaee62d64f70efedd3d53a2d67c0af5b5de59
andrewdnolan <andrew.d.nolan@maine.edu>
2023-05-30 11:17:18 -0400

    postprocess 15ka run w/ 32 workers w/ 4 GB a piece

--- expr/03_PeriodicSurge/run/crmpt12/postprocess.sh
+++ expr/03_PeriodicSurge/run/crmpt12/postprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=dask_gridding
-#SBATCH --time=03:00:00           
+#SBATCH --time=01:00:00           
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=4000M
@@ -21,21 +21,33 @@ source ./periodic_surge.sh
 
 KEY='crmpt12'
 
-# hard code betas, b/c I only want to process a subset of the files
-betas=(4.019e-04 3.831e-04 3.652e-04 3.481e-04 3.318e-04)
+# # hard code betas, b/c I only want to process a subset of the files
+# betas=(4.019e-04 3.831e-04 3.652e-04 3.481e-04 3.318e-04)
 
 # set up the dask cluter for the instance of the job array
 create_dask_cluster
 
-for beta in ${betas[@]}; do
-    # post process the surge files
-    for file in $(find ./result/${KEY}/nc/ -name "crmpt12_dx_50_TT_6000.0*B_${beta}*.nc"); do 
-        # get the base filename ,with no path info 
-        fn="${file##*/}"
-        # strip the file extension, to get the runname 
-        run_name="${fn%%.nc}"
-        # run the post processing commands
-        post_proccess
-    done 
-done
+
+base="crmpt12_dx_50_TT_15000.0_MB_-0.37_OFF_Tma_-8.5_B_1.000e-03_SP_2_QP_58.nc"
+file=$(find ./result/${KEY}/nc/ -name "${base}") 
+
+# get the base filename ,with no path info 
+fn="${file##*/}"
+# strip the file extension, to get the runname 
+run_name="${fn%%.nc}"
+# run the post processing commands
+post_proccess
+
+
+# for beta in ${betas[@]}; do
+#     # post process the surge files
+#     for file in $(find ./result/${KEY}/nc/ -name "crmpt12_dx_50_TT_6000.0*B_${beta}*.nc"); do 
+#         # get the base filename ,with no path info 
+#         fn="${file##*/}"
+#         # strip the file extension, to get the runname 
+#         run_name="${fn%%.nc}"
+#         # run the post processing commands
+#         post_proccess
+#     done 
+# done
 
