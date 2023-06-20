782aa24f0d0474812c12e1970c918dad80b67c92
andrewdnolan <andrew.d.nolan@maine.edu>
2023-06-20 17:49:09 -0400

    prep runs for 6--9ka, remove by hand simulations which failed from command list

--- expr/03_PeriodicSurge/make_experiment.py
+++ expr/03_PeriodicSurge/make_experiment.py
@@ -14,7 +14,7 @@ MEM  = '4000M'
 # fixed 2-year surge period [a]
 SP = 2
 # restart from the end of the last simulation 
-T0 = 3000.0
+T0 = 6000.0
 
 # ##################################################
 # # Reference Experiment:
@@ -34,7 +34,7 @@ cycles = [15, 30, 60]
 
 # base command used to execute a single simulation
 cmd = "./periodic_surge.py -k \"{KEY}\" -SP {SP} -QP {QP} -beta {beta:1.3e} -TT {TT} -T0 {T0} "\
-      " -RESTART crmpt12_dx_50_TT_0--3ka_MB_-0.37_OFF_Tma_-8.5_B_{beta:1.3e}_SP_2_QP_{QP}.result"
+      " -RESTART crmpt12_dx_50_TT_3--6ka_MB_-0.37_OFF_Tma_-8.5_B_{beta:1.3e}_SP_2_QP_{QP}.result"
 
 # open the commands file used by the job array
 with open(f'./run/{key}/{key}.commands', 'w') as f:
