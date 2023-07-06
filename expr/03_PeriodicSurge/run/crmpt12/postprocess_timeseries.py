55af91b82d87e2a85621e46b2d1868ea186d4240
andrewdnolan <andrew.d.nolan@maine.edu>
2023-07-06 09:36:45 -0400

    update postprocessing to work with the restarted runs

--- expr/03_PeriodicSurge/run/crmpt12/postprocess_timeseries.py
+++ expr/03_PeriodicSurge/run/crmpt12/postprocess_timeseries.py
@@ -3,8 +3,9 @@ import numpy as np
 import xarray as xr
 from os import path
 from glob import glob
-from tqdm import tqdm
+from tqdm.contrib.itertools import product
 from thermal.derived_fields import (calc_length, 
+                                    calc_volume,
                                     Variable_at_ELA, 
                                     calc_mean_enthalpy)
 
@@ -51,19 +52,21 @@ def calc_flux_at_ELA(src):
     # flux at ELA [m2 a-1], Equation (8.1) of Cuffey and Paterson
     return H_ELA*ubar_ELA
 
-def parse_params(src): 
-    """ Extract beta value and surge period from filepath and add as new dimensions
+def parse_params(fn): 
+    """ Extract beta value and surge period from filepath
     """
     # strip the file extension
-    run_name = path.basename(src.encoding['source']).split('.zarr')[0]
+    run_name = path.basename(fn).split('.zarr')[0]
     # extract the beta value and turn it into float
     beta = float(run_name.split('B')[-1].split('_')[1])
     # extract the surge recurrence, add 2 year surge period to get surge cycle
     SP = float(run_name.split('QP')[-1].split('_')[1]) + 2 
+    return np.array([beta, SP])
+
+def expand_dims(src, SP, beta): 
     # add new dimensions to dataset, which we can concat along
     src = src.expand_dims('SP').assign_coords({'SP': ('SP', [SP])})
-    src = src.expand_dims('beta').assign_coords({'beta': ('beta', [beta])})
-
+    src = src.expand_dims('beta').assign_coords({'beta': ('beta', [beta])})  
     return src
 
 @click.command()
@@ -72,11 +75,14 @@ def parse_params(src):
               type=click.Path(exists=True), required=True)
 def main(gridded_dir):
     # runname template to search, most params are hard coded but could be pass over cli 
-    run_name = "crmpt12_dx_50_TT_6000.0_MB_-0.37_OFF_Tma_-8.5_B_*_SP_2_QP_*.zarr"
+    run_name = "crmpt12_dx_50_TT_*--*ka_MB_-0.37_OFF_Tma_-8.5_B_{beta}_SP_2_QP_{QP}.zarr"
 
+    # source filepath  
+    src_fp  =  path.join(gridded_dir, run_name)
     # output filepath, where results will be written up one directory 
-    out_fp  = '/'.join(gridded_dir.split('/')[:-2])
+    out_fp  = '/'.join(gridded_dir.split('/')[:-3])
 
+    print(out_fp)
     # timeseries variables
     vars = ['initial_volume', 'relative_volume', 'percent_temperate', 'h_ELA',
             'mean_enthalpy',  'flux_across_ELA', 'u_bar_surface', 'u_bar_bed']
@@ -84,15 +90,34 @@ def main(gridded_dir):
     # empty list to store the timeseries datasets
     timeseries = []
 
-    files = sorted(glob(path.join(gridded_dir, run_name)))
+    # find all the files in the specified directory
+    files = sorted(glob(src_fp.format(beta='*',QP='*')))
 
-    for file in tqdm(files):
+    # parse the parameter combinations from the file names
+    params = np.vstack(list(map(parse_params,files)))
+
+    betas   = np.unique(params[:,0])
+    periods = np.unique(params[:,1])
+    
+    for beta, SP in product(betas, periods):
+
+        # quiescent period is surge period minus two year active phase
+        QP = SP-2
+
         # open the ith file
-        src = xr.open_zarr(file)
+        src = xr.open_mfdataset(src_fp.format(beta=f'{beta:.3e}', QP=int(QP)), 
+                                engine='zarr', data_vars='different')
 
         ##############################################################
         # Calculate diagnostic variables for analysis 
         ##############################################################
+        # recompute the volume based on the concatenated timeseires
+        volume = calc_volume(src)
+        # write the initial volume in m^2
+        src['initial_volume']  = volume.isel(t=0)
+        # write the time dependent "relative volume"
+        src['relative_volume'] = volume / volume.isel(t=0)    
+
         # calculate the area weighted average enthalpy (kJ kg-3)
         src['mean_enthalpy'] = calc_mean_enthalpy(src) / 1e3
         # calculate the flux [m2 a-1] at the ELA as a function of time
@@ -107,10 +132,10 @@ def main(gridded_dir):
         # extract the timeseries variables of interest and load into memory
         src = src[vars]
         # resample to constant sampling of dt=0.1
-        src = src.interp(t=np.linspace(0.1,  6000.1, 60001), method="linear")
+        src = src.interp(t=np.linspace(0.1,  9000.1, 90001), method="linear")
 
         # expand the dimension for the ith beta value
-        src = parse_params(src)
+        src = expand_dims(src, SP, beta)
 
         # append the preprocessed, filtered, and resampled timeseries to the list
         timeseries.append(src)
@@ -119,7 +144,7 @@ def main(gridded_dir):
     periodic_surges = xr.combine_by_coords(timeseries)
 
     # make the output filename 
-    out_fn  = 'PeriodicSurges_Timeseries_6ka.zarr'
+    out_fn  = 'PeriodicSurges_Timeseries_9ka.zarr'
     full_fn = path.join(out_fp, out_fn)
     # write the timeseries to disk
     periodic_surges.to_zarr(full_fn, mode='a')
