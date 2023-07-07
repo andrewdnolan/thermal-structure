e57d89209c6090838d911d15a3db0e4a6e5774dd
andrewdnolan <andrew.d.nolan@maine.edu>
2023-07-07 11:20:57 -0400

    9ka bisected beta runs for 30 year surges. running 9 bisected beta values, with 15 days of walltime

--- expr/03_PeriodicSurge/make_experiment.py
+++ expr/03_PeriodicSurge/make_experiment.py
@@ -6,35 +6,34 @@ from itertools import product
 # glacier identifer
 key  = 'crmpt12'
 # simulation length [yr]
-TT   = 3000
+TT   = 9000
 # walltime in dd-hh:mm:ss
-WT   = '4-4:00:00'
+WT   = '15-4:00:00'
 # memory allocation
 MEM  = '4000M'
 # fixed 2-year surge period [a]
 SP = 2
 # restart from the end of the last simulation 
-T0 = 3000.0
+T0 = 0
 
-# ##################################################
-# # Reference Experiment:
-# ##################################################
-# slip coefficients to test (3 order of magnitude)
-betas  = np.logspace(-3, -4, 9)
-# length of the surge cycle [a]
-cycles = [15, 30, 60]
-
-# ##################################################
-# # Bisect to see periodicity emerge Experiment:
-# ##################################################
+# # ##################################################
+# # # Reference Experiment:
+# # ##################################################
 # # slip coefficients to test (3 order of magnitude)
-# betas  = np.geomspace(0.0004217,0.00031623, 7)[1:-1]
+# betas  = np.logspace(-3, -4, 9)
 # # length of the surge cycle [a]
-# cycles = [30]
+# cycles = [15, 30, 60]
+
+##################################################
+# Bisect to see periodicity emerge Experiment:
+##################################################
+# slip coefficients to test (3 order of magnitude)
+betas  = np.geomspace(0.0004217,0.00031623, 11)[1:-1]
+# length of the surge cycle [a]
+cycles = [30]
 
 # base command used to execute a single simulation
-cmd = "./periodic_surge.py -k \"{KEY}\" -SP {SP} -QP {QP} -beta {beta:1.3e} -TT {TT} -T0 {T0} "\
-      " -RESTART crmpt12_dx_50_TT_0--3ka_MB_-0.37_OFF_Tma_-8.5_B_{beta:1.3e}_SP_2_QP_{QP}.result"
+cmd = "./periodic_surge.py -k \"{KEY}\" -SP {SP} -QP {QP} -beta {beta:1.3e} -TT {TT} -T0 {T0} "
 
 # open the commands file used by the job array
 with open(f'./run/{key}/{key}.commands', 'w') as f:
--- expr/03_PeriodicSurge/run/crmpt12/crmpt12_submit.sh
+++ expr/03_PeriodicSurge/run/crmpt12/crmpt12_submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --array=1-6%6
+#SBATCH --array=1-9%9
 #SBATCH --job-name=crmpt12_%A                      # base job name for the array
 #SBATCH --mem-per-cpu=4000M                        # maximum 200M per job
-#SBATCH --time=5-4:00:00                                # maximum walltime per job
+#SBATCH --time=15-4:00:00                                # maximum walltime per job
 #SBATCH --nodes=1                                  # Only one node is needed
 #SBATCH --ntasks=1                                 # These are serial jobs
 #SBATCH --mail-type=ALL                            # send all mail (way to much)
