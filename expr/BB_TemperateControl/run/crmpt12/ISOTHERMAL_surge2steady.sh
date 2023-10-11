0ba816dd24dfa9b43e42e0e3bca3b72b9fffb057
andrewdnolan <andrew.d.nolan@maine.edu>
2023-10-10 18:28:11 -0600

    misc upates

--- expr/BB_TemperateControl/run/crmpt12/ISOTHERMAL_surge2steady.sh
+++ expr/BB_TemperateControl/run/crmpt12/ISOTHERMAL_surge2steady.sh
@@ -7,18 +7,18 @@ S_dt=0.05     # surging timestep  [a]
 Q_dt=1.0      # recovery timestep [a]
 key="crmpt12" # glacier identifier
 T_ma=0.0      # Dummy value for the air temperature
-offset=-0.444    # mass balance anomoly [m a-1]
+offset=-0.444 # mass balance anomoly [m a-1]
 
 #corresponds to np.logspace(-3,-4, 9)
 betas=(1.000e-03 7.499e-04 5.623e-04 4.217e-04 3.162e-04 
        2.371e-04 1.778e-04 1.334e-04 1.000e-04)
 
-# # first surge cycle
-# for beta in ${betas[@]}; do
-#     ./surge2steady.py -k $key -dx $dx -SP $SP -QP $QP -beta $beta -SD_dt $S_dt -ST_dt $S_dt \
-#                         -QT_dt $Q_dt -QD_dt $Q_dt -z_lim 2400 -off $offset -T_ma $T_ma \
-#                         -RESTART "crmpt12_dx_50_NT_3000_dt_1.0_MB_-0.444_OFF_Tma_0.0_prog.result"
-# done
+# first surge cycle
+for beta in ${betas[@]}; do
+    ./surge2steady.py -k $key -dx $dx -SP $SP -QP $QP -beta $beta -SD_dt $S_dt -ST_dt $S_dt \
+                        -QT_dt $Q_dt -QD_dt $Q_dt -z_lim 2400 -off $offset -T_ma $T_ma \
+                        -RESTART "crmpt12_dx_50_NT_3000_dt_1.0_MB_-0.444_OFF_Tma_0.0_prog.result"
+done
 
 # Post process to zarr
 for beta in ${betas[@]}; do 
@@ -33,6 +33,7 @@ for beta in ${betas[@]}; do
                      -o "result/crmpt12/gridded/${run_name}.zarr" 
     done
 done 
+
 # # second surge cycle
 # for beta in ${betas[@]}; do
 #     ./surge2steady.py -k $key -dx $dx -SP $SP -QP $QP -beta $beta -SD_dt $S_dt -ST_dt $S_dt \
