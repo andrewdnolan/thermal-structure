c69bc9cded62d79dfafb088345a8e3de7ee24a66
Andrew Nolan <anolan@lanl.gov>
2025-01-13 20:04:43 -0700

    Update `02_surge2steady` experiment using new restart file.

--- expr/02_surge2steady/run/crmpt12/crmpt12_submit.sh
+++ expr/02_surge2steady/run/crmpt12/crmpt12_submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --array=1-9%9
 #SBATCH --job-name=crmpt12_%A                      # base job name for the array
 #SBATCH --mem-per-cpu=4000M                        # maximum 200M per job
-#SBATCH --time=48:00:00                                # maximum walltime per job
+#SBATCH --time=36:00:00                                # maximum walltime per job
 #SBATCH --nodes=1                                  # Only one node is needed
 #SBATCH --ntasks=1                                 # These are serial jobs
 #SBATCH --mail-type=END                            # send all mail (way to much)
